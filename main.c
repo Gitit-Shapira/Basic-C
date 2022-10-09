6dca129da18389a331a2d31124dc3e84c9007f03
Gitit-Shapira <64739905+Gitit-Shapira@users.noreply.github.com>
2022-10-09 11:48:10 +0300

    lesson 2 main

--- main.c
+++ main.c
@@ -1,71 +1,111 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    int x = 0,number;
-    int first_name;
-    float e = 12.0/5;
-    float y,t=5.6;
-    char c;
-    x = 14;
-    y = 4.876;
-    c = 'd';
-    printf("x = %d,y = %.3f,%.2f %c, %d\n",x,y,t,c,number);
+//    int num;
+    printf("number?\n");
+//    scanf("%d", &num);
 
-    putchar('h');
-    putchar('\n');
-    putchar(c);
-    printf("\n%c",c);
+//    if(num > 10)
+//        printf("Bigger than 10\n");
+//    if(num % 2 == 0)
+//        printf("num is even\n");
+//    if(num != 12)
+//        printf("not 12\n");
+//
+//    if( num >= 20){
+//        printf("bigger than 20\n");
+//        printf("might be 20\n");
+//    }
+//    printf("GoodBye..\n");
 
-    printf("6 + 4 = %d\n", x + 4);
-    printf("x + number = %d\n", x + number);
-    printf("x + y = %f\n", x + y);
-    printf("x  = %d\n", x );
-    printf("x = %d %c\n",x, 'h');
-    printf("- \n%d \n", 8-9);
-    printf("* %d \n", 8*9);
-    printf("/ %.1f \n",e);
-     מודולו %
-    printf("%d", 13 % 5);
+//    if (num % 2 == 0) {
+//        printf("num is even\n");
+//    } else {
+//        printf("num is odd\n");
+//    }
 
-    x = 4;
-    y = 12.4;
-    x = x + 1; // x = 5
-    printf("%d\n", x);
-    x++; // x = 6
-    printf("%d\n", x);
-    ++x; // x = 7
-    printf("%d\n", x);
-    y = x++; // y = x; x = x + 1;
-    printf("y = %.1f\n", y);
-    printf("%d\n", x);
-    y = ++x; // x = x + 1; y = x;
-    printf("y = %.1f\n", y);
-    printf("%d\n", x);
+//    if (num > 10 && num < 20){
+//        printf("10 < num < 20\n");
+//    }
+//    if(num < 3 || num > 14){
+//        printf(" num < 3 or num > 14");
+//    }
 
-    printf("%d\n",x);
-    printf("please choose an number\n");
-    scanf("%c",&c);
-    scanf("%d%f", &x, &y);
-    printf("x = %d\n%f\n",x,y);
-    putchar(c);
-    putchar('\n');
-    x = c;
-    printf("x = %d\n",x);
-    printf("%d %c", c, c);
+//    if(num){ // num != 0
+//        printf("num is not zero\n");
+//    }
+//    if(!num){ // num == 0
+//        printf("num is zero\n");
+//    }
 
-    scanf("%d:%d", &x, &number);
-    printf("x = %d\n", x);
-    printf("x = %d\n", number);
-    fflush(stdin);
-    scanf("%c", &c);
-    putchar(c);
-    putchar('\n');
-    fflush(stdin); // clear buffer
-    c = getchar(); // scanf("%c",&c);
-    putchar(c); // printf("%c", c);
-    scanf("%f", &y);
-    printf("%f", y);
+//    int x, y, q, age;
+//    scanf("%d%d%d%d", &x, &y, &q, &age);
+
+//    if (x > 5 && y == 8 && q < 1 && age == 80) {
+//        printf("hi");
+//    }
+//    if (x == 5 || y > 12 || age <= 80) {
+//        printf("bye");
+//    }
+//
+//    if (x == 5 || y == 5 && q > 3) { // 3 + 2 * 4
+//        printf("x = 5 or (y = 5 and q >3)");
+//    }
+//    if ((x == 5 || y == 5) && q > 3) { // (3 + 2) * 4
+//        printf("q > 3 and (x = 5 or y = 5)");
+//    }
+
+    int a, b, c;
+//    char even;
+    scanf("%d, %d, %d", &a, &b, &c);
+//    printf("even?\nif yes write y\n");
+//    fflush(stdin);
+//    even = getchar();
+//    if (even == '\n'){
+//        even = getchar();
+//        printf("Im in\n");
+//    }
+//    if (even == 'y') {
+//        if (a % 2 == 0)
+//            printf("a is even\n");
+//        if (b % 2 == 0)
+//            printf("b is even\n");
+//        if (c % 2 == 0)
+//            printf("c is even\n");
+//    } else {
+//        if (a % 2 != 0)
+//            printf("a is odd\n");
+//        if (b % 2) // b % 2 != 0
+//            printf("b is odd\n");
+//        if (c % 2 == 1)
+//            printf("c is odd\n");
+//    }
+
+//    if( even == 'y' && a % 2 ==0 || even != 'y' && a % 2 != 0)
+//        printf("%d\n", a);
+//    if( even == 'y' && b % 2 ==0 || even != 'y' && b % 2 != 0)
+//        printf("%d\n", b);
+//    if( even == 'y' && c % 2 ==0 || even != 'y' && c % 2 != 0)
+//        printf("%d\n", c);
+
+//    if(a > 3)
+//        printf("a is bigger than 3\n");
+//    else if(b % 2 == 0)
+//        printf("b is even and a <= 3\n");
+//    else if(c != 12)
+//        printf("c != 12 and b is odd\n");
+//    else
+//        printf("a <= 3 b is odd c = 12");
+
+    c = a > b ? a : b;
+    /*
+     if ( a > b)
+        c = a
+    else
+        c = b
+     */
+    printf("%c\n", a != b ? 'y' : 'n');
+    printf("%d", c);
 
 
     return 0;
